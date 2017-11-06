c66416cd5697c17813a95f72d30bb7bb862ca147
Christian Anglo <canglo@academic.rrc.ca>
2017-11-06 05:13:30 +0000

    Added the association between the product and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  belongs_to :category #there is a category_id int key in product table
+
   validates :name, :description, :size, :condition, :price, :stock_quantity, presence: true
 end
