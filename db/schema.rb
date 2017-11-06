240e137d31921bac291a28c61b4994edcd4ac5d8
Christian Anglo <canglo@academic.rrc.ca>
2017-11-06 05:36:37 +0000

    Whitelisted the active admin and added validation on condition model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106051855) do
+ActiveRecord::Schema.define(version: 20171106052750) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 20171106051855) do
   end
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "conditions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
