b90f8a1a21b7c8b2cc553c610aef99adfec76688
Christian Anglo <canglo@academic.rrc.ca>
2017-11-14 22:39:05 +0000

    Generated products controller with index and show views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'products#index'
+
+  get 'products/show'
+
   get 'about', to: 'pages#about', as: 'about'
 
   get 'contact', to: 'pages#contact', as: 'contact'
