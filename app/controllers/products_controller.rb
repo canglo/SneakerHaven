687c11844c2d124b1149cfab465b069678d56e7f
Christian Anglo <canglo@academic.rrc.ca>
2017-11-15 22:47:37 +0000

    Added a keyword search function.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,13 @@ class ProductsController < ApplicationController
 
   def show
   end
+
+  def search
+
+  end
+
+  def search_results
+    keywords = "%" + params[:search_keywords] + "%"
+    @found_products = Product.where("name LIKE ?", keywords).page(params[:page]).per(6)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact', as: 'contact'
 
+  get 'search' => 'products#search', as: 'search'
+  get 'search_results' => 'products#search_results', as: 'search_results'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
