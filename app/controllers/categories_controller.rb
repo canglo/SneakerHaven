b8072368913b0bc82145ee87eb78ad50de6b0b15
Christian Anglo <canglo@academic.rrc.ca>
2017-11-21 03:56:03 +0000

    Fixed some code on the search.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
   def brand_category
+    @categories = Category.all
     @products = Product.where(category_id: params[:id]).order(id: :desc).
                 page(params[:page]).per(6)
   end
