3ff661a429b85a0b3da609402dcbd1b0ec612f67
Christian Anglo <canglo@academic.rrc.ca>
2017-11-23 04:49:34 +0000

    Added subtotal and total columns in orders table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123043819) do
+ActiveRecord::Schema.define(version: 20171123044749) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 20171123043819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
+    t.decimal "subtotal"
+    t.decimal "total"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
