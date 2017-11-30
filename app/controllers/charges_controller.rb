a297de3f90464962ca7e96e676ed8061d3a680cf
Christian Anglo <canglo@academic.rrc.ca>
2017-11-30 06:23:25 +0000

    Finished the invoice, it's now showing customer details and order details.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,16 @@
 class ChargesController < ApplicationController
   def index
     @line_items = current_order.line_items
+    @user = current_customer
+
+    @line_items.each do |f|
+      @subtotal = current_order.subtotal
+      @gst = (@user.province.gst / 100) * @subtotal
+      @pst = (@user.province.pst / 100) * @subtotal
+      @hst = (@user.province.hst / 100) * @subtotal
+      @total = @subtotal + @gst + @pst + @hst
+    end
+
+
   end
 end
